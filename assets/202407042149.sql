e2747b1f67a3f9a575327ae31427823910781319
SL <No8blackball@163.com>
2024-07-04 22:35:49 +0800

    fix: Upgrade message protocol, add message extension topping function and the ability to send expired messages

--- assets/202407042149.sql
+++ assets/202407042149.sql
@@ -1,3 +1,3 @@
 ALTER TABLE 'message' add column 'expire_time' BIGINT DEFAULT 0;
 ALTER TABLE 'message' add column 'expire_timestamp' BIGINT DEFAULT 0;
-ALTER TABLE 'message_extra' ADD COLUMN is_pinned int DEFAULT 0;
+ALTER TABLE 'message_extra' ADD COLUMN 'is_pinned' int DEFAULT 0;
